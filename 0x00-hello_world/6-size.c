c4f52ffead73287b2d4ae17082ad48367b1c32f8
ymcastellar <yoycas@hotmail.com>
2020-02-05 21:49:41 +0000

    added 6_size file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - this function prints size of characters
+* Return: 0
+*/
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+return (0);
+}
