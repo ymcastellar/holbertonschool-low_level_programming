c1b57c644c858429ddee98ce17b6797f03b1b1fe
ymcastellar <yoycas@hotmail.com>
2020-02-05 20:59:02 +0000

    my first .c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-int main()
+/** 
+* main - this is like a hello world
+*/
+int main (void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 
-	return(0);
+	return (0);
 }
