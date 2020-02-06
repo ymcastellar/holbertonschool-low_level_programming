152814e0215638b1f77837c3ab819d8e94050064
ymcastellar <yoycas@hotmail.com>
2020-02-06 20:44:20 +0000

    added 1-last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ else if ((n % 10) == 0)
 {
 printf("Last digit of %i is %i and is 0\n", n, (n % 10));
 }
-else
+else if ((n % 10) < 6 && (n % 10) != 0)
 {
 printf("Last digit of %i is %i and is less than 6 and 0\n", n, (n % 10));
 }
