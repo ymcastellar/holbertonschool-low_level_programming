2dd53123559f449cb5033e0ce12c354a35d96ef4
ymcastellar <yoycas@hotmail.com>
2020-02-06 21:11:26 +0000

    added 2.alphaabet file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,15 @@
 * main - alphabet
 * Return: always 0
 */
+
 int main(void)
 {
 int letter;
 
-for (letter = 97; i <= 122; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 putchar(letter);
+}
 putchar('\n');
 return (0);
 }
