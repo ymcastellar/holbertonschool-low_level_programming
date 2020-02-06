650c9b477a861c2b30e7599ded60bf34ba0b17fc
ymcastellar <yoycas@hotmail.com>
2020-02-06 22:35:55 +0000

    added 7-smile file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - tebahpla
+* Return: always 0
+*/
+
+int main(void)
+{
+int letter;
+
+for (letter = 122; letter > 96; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
