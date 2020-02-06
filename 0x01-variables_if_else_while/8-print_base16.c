cf569213101881163949e6447081a215b2d20240
ymcastellar <yoycas@hotmail.com>
2020-02-06 23:19:10 +0000

    added 8-hexa file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,16 +9,16 @@ int main(void)
 {
 int n1, n2;
 n1 = 49;
-n2 = 65;
 while (n1 < 58)
 {
 putchar(n1);
 n1++;
 }
-while (n2 < 71)
+n1 = 65;
+while (n1 < 71)
 {
-putchar(n2);
-n2++;
+putchar(n1);
+n1++;
 }
 putchar('\n');
 return (0);
