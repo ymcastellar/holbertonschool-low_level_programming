342c81c252132501e25c2dffd7286f2f70c5d1b4
ymcastellar <yoycas@hotmail.com>
2020-02-07 00:19:48 +0000

    added 10-comb2 file

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - print comb2
+* Return: 0
+*/
+
+int main(void)
+{
+int n1, n2;
+
+for (n1 = 48; n1 < 58; n1++)
+{
+for (n2 = 48; n2 < 58; n2++)
+{
+putchar(n1);
+putchar(n2);
+if (n1 == 57 && n2 == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}		
+}
+putchar(' ');
+return (0);
+}
