e2c975aac513aaf4397879c18a46cbe235476929
ymcastellar <yoycas@hotmail.com>
2020-02-10 23:04:30 +0000

    dde alphabetx10 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include "2-main.c"
+
+/**
+*print_alphabet_x10 - 2-alphabet
+*Return: void
+*/
+
+
+void print_alphabet_x10(void)
+{
+char i;
+
+i = 0;
+
+while (i <= 9)
+{
+char letter;
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+i++;
+}
+}
