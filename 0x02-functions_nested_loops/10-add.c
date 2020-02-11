2964b0c8bce12e24da00d70ec46d24734efc4820
ymcastellar <yoycas@hotmail.com>
2020-02-11 20:49:27 +0000

    task 10 add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ *@n1: first parameter
+ *@n2: second parameter
+ * Return: result.
+ */
+
+int add(int n1, int n2)
+{
+int res;
+
+res = n1 + n2;
+return (res);
+}
