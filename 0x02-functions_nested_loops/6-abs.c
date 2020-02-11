37214ab32e2b7bbd6a198bdc99a093112f342e58
ymcastellar <yoycas@hotmail.com>
2020-02-11 16:25:28 +0000

    added absolute file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+*_abs - adbsolute number
+*@n: parameter
+*Return: n - number
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-(n));
+}
+else
+{
+return (n);
+}
+}
+
