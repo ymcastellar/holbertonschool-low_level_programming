685662f57a19b4eb45671558b6874c667425d100
ymcastellar <yoycas@hotmail.com>
2020-02-13 16:13:03 +0000

    added files for 3 taks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*print_numbers - print 0 to 9
+*Return: void
+*/
+
+void print_numbers(void)
+{
+int n;
+n = 48;
+while (n <= 57)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
