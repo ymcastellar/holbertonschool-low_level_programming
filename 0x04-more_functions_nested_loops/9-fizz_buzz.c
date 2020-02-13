5d79d74817921e5e35252df853dd86740e3b4960
ymcastellar <yoycas@hotmail.com>
2020-02-13 22:42:28 +0000

    added files task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - task 9
+*Return: 0
+*/
+
+
+int main(void)
+{
+int i, mul3, mul5;
+for (i = 1 ; i <= 100 ; i++)
+{
+mul3 = i % 3;
+mul5 = i % 5;
+if (mul3 == 0)
+{
+printf("%s ", "Fizz");
+}
+else if (mul5 == 0)
+{
+printf("%s ", "Buzz");
+}
+else
+{
+printf("%i ", i);
+}
+}
+printf("\n");
+return (0);
+}
