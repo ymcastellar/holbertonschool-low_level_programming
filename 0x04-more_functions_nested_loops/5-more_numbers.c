197ad6f9e989b3af18535771a64b1b44733ec2e9
ymcastellar <yoycas@hotmail.com>
2020-02-13 18:48:50 +0000

    added files for task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*more_numbers - task 5
+*Return: void
+*/
+
+void more_numbers(void)
+{
+int n;
+for (n = 0; n <= 14 ; n++)
+{
+if(n > 9)
+{
+_putchar(n / 10 + '0');
+}
+_putchar(n % 10 + '0');
+}
+_putchar('\n');
+}
