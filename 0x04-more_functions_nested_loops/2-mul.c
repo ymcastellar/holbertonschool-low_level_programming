e4eca627ca5fc2b1bdaa8a52aa92163f9a4686e3
ymcastellar <yoycas@hotmail.com>
2020-02-13 15:46:43 +0000

    added files for 2 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*mul -multiplies two integers.
+*@a: parameter one
+*@b: parameter two
+*Return: result of multiplies
+*/
+
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
