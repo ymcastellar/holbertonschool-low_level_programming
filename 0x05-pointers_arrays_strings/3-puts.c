eb985fcd22c7a47dc61f3a5e002b9eb6f6e7a2aa
ymcastellar <yoycas@hotmail.com>
2020-02-18 02:54:10 +0000

    added files for task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*_puts -  task 3
+*@str: char parameter
+*Return: string
+*/
+void _puts(char *str)
+{
+while (*str!='\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n'); 
+}
