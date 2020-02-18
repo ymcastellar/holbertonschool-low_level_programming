744ed3765e75d69ceeeb3d0978ffe83c21a0c2b3
ymcastellar <yoycas@hotmail.com>
2020-02-18 17:04:25 +0000

    added files for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+*print_rev - print reverwse string
+*@s: parameter sring
+*Return: void
+*/
+
+void print_rev(char *s)
+{
+int cont, cont1;
+
+cont = 0;
+
+while (*(s + cont) != '\0')
+{
+cont++;
+}
+
+cont1 = cont;
+
+for(cont1-- ; cont1 >= 0 ; cont1--)
+{
+_putchar(*(s + cont1));
+} 
+_putchar('\n');
+}
