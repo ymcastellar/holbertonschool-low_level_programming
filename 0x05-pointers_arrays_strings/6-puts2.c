dcf47ff48cb7792c1f89264f4e8c870e9557359d
ymcastellar <yoycas@hotmail.com>
2020-02-18 21:01:09 +0000

    added files for task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,16 @@
 
 void puts2(char *str)
 {
-
+int cont;
+cont = 0;
 while (*str != '\0')
 {
-if((*str % 2) == 0)
+if((cont % 2) == 0)
 {
 _putchar(*str);
 }
 str++;
+cont++;
 }
 _putchar('\n');
 }
