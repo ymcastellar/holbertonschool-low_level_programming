81177bab7e106fb609c417c3e9d08959b7db0662
ymcastellar <yoycas@hotmail.com>
2020-02-18 23:14:10 +0000

    added files for task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - check the code for Holberton School students.
+ *@a: parameter int
+ *Qn: parameter int
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+
+if (i == n - 1)
+{
+printf(" ");
+}
+else
+{
+printf(", ");
+}
+
+}
+printf("\n");
+}
