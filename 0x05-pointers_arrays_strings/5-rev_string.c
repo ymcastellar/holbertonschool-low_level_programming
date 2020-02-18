75f534b3545f264a1da73971b19b4cfcfc250dfe
ymcastellar <yoycas@hotmail.com>
2020-02-18 19:40:00 +0000

    added files for task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+*rev_string - reverses a string
+*@s: string parameter
+*Return. void
+*/
+
+void rev_string(char *s)
+{
+
+char temp;
+int b, cont; 
+
+temp = s[0];
+cont = 0;
+b = 0;
+while (s[cont] != '\0')
+{
+cont++;
+}
+
+for (b = 0 ; b <= cont ; b++)
+{
+cont--;
+temp = s[b] ;
+s[b] = s[cont];
+s[cont] = temp;
+}
+
+}
