c2185b37498812a27b8f4706c0719228e1545512
ymcastellar <yoycas@hotmail.com>
2020-02-18 01:14:30 +0000

    added files task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+*reset_to_98-that takes a pointer to an int as parameter and updates the value it points to to 98.
+*@n: int pointer
+*Return: void
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,6 @@
+
+
+
+void reset_to_98(int *n);
+
+
