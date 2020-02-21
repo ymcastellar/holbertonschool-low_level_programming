39138443376a8b3a525a2de169d82d6cf2934cba
ymcastellar <yoycas@hotmail.com>
2020-02-21 04:57:24 +0000

    added files for taks 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,7 +1,7 @@
 /**
-* rot13 - conver string
-* @s: string
-* Return: string
+*rot13 - conver string
+*@s: string
+*Return: string
 */
 
 char *rot13(char *s)
@@ -13,6 +13,7 @@ char *letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char *encrypt = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 for (i = 0; s[i] != '\0'; i++)
+{
 for (j = 0; letters[j] != '\0'; j++)
 {
 if (s[i] == letters[j])
@@ -20,6 +21,7 @@ if (s[i] == letters[j])
 s[i] = encrypt[j];
 break;
 }
-
+}
+}
 return (s);
 }
