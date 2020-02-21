9250ef1d4be4f0071886b8b46990c525161fa4a6
ymcastellar <yoycas@hotmail.com>
2020-02-21 04:50:12 +0000

    added files for taks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,20 +5,49 @@
 */
 char *cap_string(char *s)
 {
-int i;
+int i = 0, s;
 
-for (i = 1; s[i] != '\0'; i++)
+while (s[i] != '\0')
 {
-if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == '.') && (s[i] > 'a' && s[i] < 'z'))
+
+if (char_seperator(s[i]) == 1)
 {
-s[i] = s[i] - 32;
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] = s[i + 1] - 32;
+}
+}
+i
+i++;
+}
+
+return (s);
+
+}
+
+/**
+* char_seperator - validatea seperator
+* @c: parameter to check
+* Return: true for find seprator and false if it do not found
+*/
+
+int char_seperator(char c)
+{
+int i = 0, leng_s;
+int leng_s = 13;
+char seperators[] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
+
+while (seperators < leng_s)
+{
+if (c == seperators[i++])
+{
+return (1);
 }
 else
 {
-s[i] = s[i];
+return (0); 
 }
 }
-
 
 return (s);
 }
