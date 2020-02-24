e9e8f57cfd7eff7d3760b2f2e411c7e829ed279a
ymcastellar <yoycas@hotmail.com>
2020-02-24 20:32:16 +0000

    added files for task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+*_strchr - locate character in a string
+*@s: char parameter
+*@c: char parameter
+*Return: s
+*/
+
+char *_strchr(char *s, char c)
+{
+
+
+for ( ; s != '\0' ; s++)
+{
+if (*s == c)
+{
+return (s);
+}
+}
+
+if (*s == c)
+{
+return (s);
+}
+
+return(NULL);
+
+}
