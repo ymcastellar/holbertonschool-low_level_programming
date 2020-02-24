6099bedcd56a1fd37f072f42e4e3a451b1f969c0
ymcastellar <yoycas@hotmail.com>
2020-02-24 23:49:08 +0000

    added files for task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+*_strstr - print a substring
+*@haystack:parameter
+*@needle: parameter
+*Return: substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+register char *a, *b;
+
+b = needle;
+
+if (*b == 0)
+{
+return (haystack);
+}
+for ( ; *haystack != 0; haystack++)
+{
+if (*haystack != *b)
+{
+continue;
+}
+
+a = haystack;
+while (1)
+{
+if (*b == 0)
+{
+return (haystack);
+}
+if (*a++ != *b++)
+{
+break;
+}
+}
+b = needle;
+}
+
+return (NULL);
+
+}
