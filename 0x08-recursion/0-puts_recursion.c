c8ee3f15aaadd40b57a1ad3f04ea19228f469c03
ymcastellar <yoycas@hotmail.com>
2020-02-26 20:23:48 +0000

    added files for task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*_puts_recursion - print a string
+*@s: parameter
+*Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+
+}
