87b217726cb2e793bfb222dc26047b17f3bb5739
ymcastellar <yoycas@hotmail.com>
2020-02-27 04:54:44 +0000

    added files for task100

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+* wildcmp - compare two strings
+* @s1: string parameter
+* @s2: string parameter
+* Return: int value
+*/
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s2 == '*')
+{
+if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+return (1);
+if (*(s1 + 1) != '\0' && *s2 == '\0')
+return (0);
+}
+return (0);
+}
