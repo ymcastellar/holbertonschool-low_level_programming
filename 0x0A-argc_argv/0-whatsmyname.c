56583d103d43d1ae95693a333b3f0e28d3f132c0
ymcastellar <yoycas@hotmail.com>
2020-02-29 21:14:41 +0000

    added files for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,21 +9,12 @@
 
 int main(int argc, char *argv[])
 {
-int count;
 
+if (argc == 1)
+{
 printf("%s\n", argv[0]);
+}
 
-if (argc > 1)
-{
-for (count = 1; count < argc; count++)
-{
-printf("argv[%d] = %s\n", count, argv[count]);
-}
-}
-else
-{
-printf("The command had no other arguments.\n");
-}
 
 return (0);
 }
