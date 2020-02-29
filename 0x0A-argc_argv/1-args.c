03cf2c998a86d563b8c3dca0ab04abb1ad7f0e0d
ymcastellar <yoycas@hotmail.com>
2020-02-29 21:29:14 +0000

    added files fortask 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - print argc
+*@argc: argument count
+*@argv: argument veector
+*Return: always 0
+*/
+
+int main (int argc, char __attribute__((unused)) *argv[])
+{
+
+
+printf("%d\n", argc - 1);
+
+
+return (0);
+}
