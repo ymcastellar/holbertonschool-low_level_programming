cf788b6139a863da4db69629defcc0479aad8738
ymcastellar <yoycas@hotmail.com>
2020-02-29 17:08:56 +0000

    added files for task 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
+/**
+*main - main project
+*Return: always 0
+*/
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
