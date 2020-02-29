f1070eb637c7f825033f9fbcac306313f6c163fd
ymcastellar <yoycas@hotmail.com>
2020-02-29 23:23:35 +0000

    added files for task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+*main - print multiplies two numbers
+*@argc: argument count
+*@argv: argument veector
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+
+int count, sum;
+
+sum = 0;
+
+if (argc >= 1)
+{
+for (count = 1; count < argc; count++)
+{
+if(isdigit(atoi(argv[count])))
+{
+if(atoi(argv[count]) > 0 )
+{
+sum += atoi(argv[count]);
+}
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", sum);
+}
+else if (argc == 0)
+{
+printf("Error\n");
+return (0);
+}
+
+
+return (0);
+}
