efdc0544aa1aba45c93bab8680d2ac1aea6d01c9
ymcastellar <yoycas@hotmail.com>
2020-03-06 02:28:20 +0000

    added files for task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+/**
+* _calloc - function that allocates memory for an array
+*@nmemb: Array parameter.
+*@size: Size of array.
+*
+*Return: pointer to the allocated memory.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *ar;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+
+ar = malloc(nmemb * size);
+
+if (ar == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < (nmemb * size); i++)
+{
+*(ar + i) = 0;
+}
+
+return (ar);
+}
