f5380df42dd9f98adf3961fd3b80ef63dc995726
ymcastellar <yoycas@hotmail.com>
2020-03-06 02:04:59 +0000

    added files for task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenate two strings
+ * @s1: string parameter
+ * @s2: string parameter
+ * @n: number parameter
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j;
+char *ptr;
+
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+for (i = 0; s1[i] != '\0'; i++)
+;
+
+for (j = 0; s2[j] != '\0'; j++)
+;
+
+if (n >= j)
+n = j;
+
+ptr = malloc(1 + (i + n) * sizeof(*ptr));
+
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+ptr[i] = s1[i];
+}
+
+for (j = 0; j < n; j++, i++)
+{
+ptr[i] = s2[j];
+}
+
+ptr[i] = '\0';
+
+return (ptr);
+}
