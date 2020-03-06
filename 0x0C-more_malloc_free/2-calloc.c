e7eec7fdcbae5b92fa831bc7b09781d72b731f68
ymcastellar <yoycas@hotmail.com>
2020-03-06 02:45:23 +0000

    added files for task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,26 +9,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-
 unsigned int i;
 char *ar;
 
 if (nmemb == 0 || size == 0)
-{
 return (NULL);
-}
 
 ar = malloc(nmemb * size);
 
 if (ar == NULL)
-{
 return (NULL);
-}
 
 for (i = 0; i < (nmemb * size); i++)
-{
 *(ar + i) = 0;
-}
 
 return (ar);
 }
