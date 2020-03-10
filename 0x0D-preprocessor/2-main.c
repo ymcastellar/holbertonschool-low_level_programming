7e9056fe7dd2455aca45a2fc318afc0aa4378fab
ymcastellar <yoycas@hotmail.com>
2020-03-10 00:31:59 +0000

    added files for task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,3 +1,12 @@
-#ifndef __FILE__
-#define __FILE__ 
-#endif /* __FILE__ */
+#include <stdio.h>
+/**
+ * main - print filename.
+ *
+ * Return:  returns 0.
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
