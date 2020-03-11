a7a9244380260deca709a59dc4d251aa8c0cca83
ymcastellar <yoycas@hotmail.com>
2020-03-11 21:56:21 +0000

    added files for task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,40 @@
+#include "function_pointers.h"
+
+/**
+*int_index - searches for an integer.
+*@array: parameter
+*@size: parameter
+*@cmp: parameter
+*Return: int
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+int i;
+
+if (size <= 0)
+{
+return (-1);
+}
+
+if (array != NULL && cmp != NULL)
+{
+
+for (i = 0 ; i < size ; i++)
+{
+
+if(cmp(array[i]) == 1)
+{
+return (i);
+}
+}
+if(cmp(array[i]) == 0)
+{
+return (-1);
+}
+}
+
+return (-1);
+
+}
