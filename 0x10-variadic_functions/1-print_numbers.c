00373cc860c59cee2d0e6eb090ffc20191144ce8
ymcastellar <yoycas@hotmail.com>
2020-03-13 20:04:46 +0000

    added files for task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+*print_numbers - print numbers
+*@separator: parameter
+*@n: n parameter
+*Return: numbers
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list list;
+
+unsigned int i;
+
+va_start(list, n);
+
+for (i = 0 ; i < n ; i++)
+{
+
+if(i != (n - 1) && separator != NULL)
+{
+printf("%i%s", va_arg(list, unsigned int), separator);
+}
+else
+{
+printf("%i", va_arg(list, unsigned int));
+}
+
+}
+
+printf("\n");
+
+}
