b95f1fa77236928b423697f3ade43fe4f7e34bec
ymcastellar <yoycas@hotmail.com>
2020-03-19 20:58:38 -0500

    fixed betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,13 +8,13 @@
 
 void free_list(list_t *head)
 {
-    list_t *aux;
+	list_t *aux;
 
-    while (head != NULL)
-    {
-        aux = head->next;
-        free(head->str);
-        free(head);
-        head = aux;
-    }
+	while (head != NULL)
+	{
+		aux = head->next;
+		free(head->str);
+		free(head);
+		head = aux;
+	}
 }
