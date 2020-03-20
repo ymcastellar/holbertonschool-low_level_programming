309b65198452a8b14dead81e114a8d9e4d1443ea
ymcastellar <yoycas@hotmail.com>
2020-03-19 20:34:01 -0500

    modif files for task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,46 +1,34 @@
 #include "lists.h"
 
 /**
- *add_node - print a linked list
+ *add_node - adds a new node
  *@head: parameter
  *@str: parameter
- *Return: number of nodes
+ *Return: addres of the new element
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int i;
-	list_t *new_n;
-	list_t *last;
+	int i;
+	list_t *next_node;
 
 	i = 0;
 
 	if (!str)
 		return (NULL);
 
-	new_n = malloc(sizeof(list_t));
-	last = *head;
-
-	if (new_n == NULL)
+	next_node = malloc(sizeof(list_t));
+	if (next_node == NULL)
 		return (NULL);
 
 	while (str[i] != 0)
 		i++;
 
-	new_n->str = strdup(str);
-	new_n->len = i;
-	new_n->next = NULL;
+	next_node->str = strdup(str);
+	next_node->len = i;
+	next_node->next = (*head);
 
-	if (*head == NULL)
-	{
-		*head = new_n;
-		return (new_n);
-	}
+	*head = next_node;
 
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = new_n;
-
-	return (new_n);
+	return (next_node);
 }
