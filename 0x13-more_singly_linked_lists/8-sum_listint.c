f73884514599b029c9786dddf6117b07f0438e30
ymcastellar <yoycas@hotmail.com>
2020-03-24 11:11:39 -0500

    added files for task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *sum_listint - sum of all the data (n)
+ *@head: parameter first node
+ *Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *first;
+	int sum;
+
+	first = head;
+
+	if (first == NULL)
+		return (0);
+
+	while (first)
+	{
+		sum += first->n;
+		first = first->next;
+	}
+
+	return (sum);
+
+
+}
