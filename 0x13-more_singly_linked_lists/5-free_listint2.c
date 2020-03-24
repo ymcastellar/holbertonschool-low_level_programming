5ebcc8780d5bba44026a285b7da58ea2b21bd459
ymcastellar <yoycas@hotmail.com>
2020-03-23 22:06:59 -0500

    added files for task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - function that frees a list
+ *@head: parameter
+ *Return:  void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
