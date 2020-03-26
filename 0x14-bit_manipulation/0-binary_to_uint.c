cca68071c03e693f0135eb9cd62864ef0adcd47d
ymcastellar <yoycas@hotmail.com>
2020-03-26 14:12:41 -0500

    added files for task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *int binary_to_uint - convert from binary to a unsigned int
+ *@b: pointing to a string
+ *Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	if (*b)
+	{
+		unsigned int val;
+
+		for (val = 0; *b; b++)
+		{
+			if (*b == '1')
+				val = (val << 1) | 1;
+			else if (*b == '0')
+				val <<= 1;
+			else
+				break;
+		}
+
+		return (val);
+	}
+
+	return (0);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,12 @@
+#ifndef __HOLBERTON__
+#define __HOLBERTON__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stddef.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
