19c966f49a7e0b71f979f7c167a5526545123f32
ymcastellar <yoycas@hotmail.com>
2020-03-31 17:00:17 -0500

    added files for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ *create_file -create a file
+ *@filename: name of the file
+ *@text_content: text to put in the file
+ *Return: 1-succes, -1-fails.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fileWrite;
+
+	int *buf;
+
+	if (!filename)
+		return (0);
+
+	buf = malloc(sizeof(text_content));
+
+	if (!buf)
+		return (0);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd < 0)
+	{
+		free(buf);
+		return (-1);
+	}
+
+	fileWrite = write(fd, text_content, (size_t)buf);
+	close(fd);
+
+	if (fileWrite < 0)
+		return(-1);
+
+	return(1);
+}
