03173fd0894df2d28a0232c3a548ea2876978d3e
ymcastellar <yoycas@hotmail.com>
2020-03-31 17:33:37 -0500

    modif file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i]; i++)
 			;
 		fileWrite = write(fd, text_content, i);
-		if (w == -1)
+		if (fileWrite == -1)
 			return (-1);
 	}
 
