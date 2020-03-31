17ea159138b8a6408cfb7a1efd4cd0be6d385ba4
ymcastellar <yoycas@hotmail.com>
2020-03-31 17:28:11 -0500

    modif file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fileWrite, i = 0;
+	int fd, fileWrite, i;
 
 	if (!filename)
 		return (-1);
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	while (text_content[i] != '\0')
-		i++;
+	for (i = 0; text_content[i]; i++)
+            ;
 
 	fileWrite = write(fd, text_content, i);
 
