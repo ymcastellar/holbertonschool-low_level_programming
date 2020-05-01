4e25f1a330d7f0f7964c0b5311e9d7d872697149
ymcastellar <yoycas@hotmail.com>
2020-05-01 17:08:56 -0500

    added files for task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,6 +9,27 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t i;
+	dlistint_t *aux = *(head);
 
+	if (head == NULL)
+		return (NULL);
+
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	*(head) = new;
+	new->n = n;
+	new->prev = NULL;
+	if (aux == NULL)
+		new->next = NULL;
+	else
+	{
+		new->next = aux;
+		aux->prev = *(head);
+		aux = NULL;
+	}
+
+	return (*head);
 }
