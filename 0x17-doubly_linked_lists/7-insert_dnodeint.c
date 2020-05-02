f4ede4fa4dc1600e83594b44d4aa791e669e3a33
ymcastellar <yoycas@hotmail.com>
2020-05-02 16:23:12 -0500

    fixed code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,8 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *node_temp;
 	unsigned int i;
 
-	if (!h)
-		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	node_temp = *h;
